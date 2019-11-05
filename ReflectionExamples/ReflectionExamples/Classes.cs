5ce4599daa1c35a28e27dd83772997798394b5e6
Akif <akifmt@gmail.com>
2019-11-05 13:42:18 +0300

    Added project classes and test console app

---
+++ ReflectionExamples/ReflectionExamples/Classes.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionExamples
+{
+
+    public static class Statics
+    {
+
+        public static AllTypesData Data = new AllTypesData();
+
+        public static Kategori OrnekVeri = new Kategori
+        {
+            KategoriId = 1,
+            KategoriAdi = "Arabalar",
+            KategoriDetayi = "Super Arabalar",
+            OnemliUrun = new Urun { Id = 1, UrunAdi = "Mercedes 1", UrunDetayi = "Bu model tekerleklidir." },
+            Urunler = new List<Urun>
+            {
+                new Urun { Id = 11, UrunAdi = "Mercedes 11", UrunDetayi = "Bu modelde cam vardır." },
+                new Urun { Id = 12, UrunAdi = "Mercedes 12", UrunDetayi = "Bu modele direksiyon ekledik." },
+            }
+        };
+
+
+    }
+
+
+
+    public class AllTypesData
+    {
+        public System.Boolean PropBoolean { get { return true; } } //bool	
+        public System.Byte PropByte { get { return byte.MaxValue; } } //byte	
+        public System.SByte PropSByte { get { return sbyte.MaxValue; } } //sbyte	
+        public System.Char PropChar { get { return 'A'; } } //char	
+        public System.Decimal PropDecimal { get { return decimal.MaxValue; } } //decimal	
+        public System.Double PropDouble { get { return double.MaxValue; } } //double	
+        public System.Single PropSingle { get { return Single.MaxValue; } } //float	
+        public System.Int32 PropInt32 { get { return Int32.MaxValue; } } //int		
+        public System.UInt32 PropUInt32 { get { return uint.MaxValue; } } //uint	
+        public System.Int64 PropInt64 { get { return Int64.MaxValue; } } //long	
+        public System.UInt64 PropUInt64 { get { return UInt64.MaxValue; } } //ulong	
+        public System.Object PropObject { get { return new object(); } } //object	
+        public System.Int16 PropInt16 { get { return Int16.MaxValue; } } //short	
+        public System.UInt16 PropUInt16 { get { return UInt16.MaxValue; } } //ushort	
+        public System.String PropString { get { return "ABC"; } } //string	
+
+        // Class - User defined
+        public Urun PropUrun { get { return new Urun { Id = 1, UrunAdi = "ad", UrunDetayi = "detay" }; } }
+
+        // System.Collections.Generic - include Built-in Type
+        public List<int> PropListInt { get { return new List<int> { 1, 2, 3 }; } } //List<int>	
+        public Dictionary<int, string> PropDictionaryIntStr { get { return new Dictionary<int, string> { { 1, "A" }, { 2, "B" } }; } } //Dictionary<int,string>	
+
+        // System.Collections.Generic - include Class
+        public List<Urun> PropListUrun { get { return new List<Urun> { new Urun { Id = 1, UrunAdi = "aaa", UrunDetayi = "ddd" } }; } } //List<class>	
+
+    }
+
+    public class Kategori
+    {
+        public int KategoriId { get; set; }
+        public string KategoriAdi { get; set; }
+        public string KategoriDetayi { get; set; }
+        public Urun OnemliUrun { get; set; }
+        public List<Urun> Urunler { get; set; }
+    }
+
+    public class Urun
+    {
+        public int Id { get; set; }
+        public string UrunAdi { get; set; }
+        public string UrunDetayi { get; set; }
+    }
+
+}
---
+++ ReflectionExamples/ReflectionExamples/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ReflectionExamples
+{
+    class Program
+    {
+        public const string PROJECT_NAME = "ReflectionExamples";
+
+
+        static void Main(string[] args)
+        {
+            //WritePropertiesonBaseClass(Statics.OrnekVeri);
+
+            WritePropertiesonBaseClass(Statics.Data);
+
+            Console.ReadKey();
+        }
+
+
+
+        static void WritePropertiesonBaseClass(object obj)
+        {
+            Type typeBase = obj.GetType();
+            PropertyInfo[] propertyInfosBase = typeBase.GetProperties();
+
+            Console.WriteLine(string.Format("Base Object: {0}", typeBase.Name));
+
+            foreach (var propertyInfoBase in propertyInfosBase)
+            {
+                Type propTypeBase = propertyInfoBase.PropertyType;
+
+                switch (propTypeBase.Namespace)
+                {
+                    case "System":
+                        // Built-in types
+                        ProcessingBuiltinTypes(propertyInfoBase, obj);
+                        break;
+
+                    case PROJECT_NAME:
+                        // User defined classes
+                        ProcessingClassTypes(propertyInfoBase, obj);
+
+                        break;
+
+                    case "System.Collections.Generic":
+                        // Collection types
+                        ProcessingGenericCollection(propertyInfoBase, obj);
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+        static void ProcessingBuiltinTypes(PropertyInfo propertyInfo, object data, string prefix = "|-")
+        {
+            Console.WriteLine(string.Format("{0}{1}({2}) \t\tValue:{3}", prefix, propertyInfo.Name, propertyInfo.PropertyType.Name, propertyInfo.GetValue(data)));
+        }
+
+        /// <summary>
+        /// for inner Collections
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="value"></param>
+        /// <param name="index"></param>
+        /// <param name="prefix"></param>
+        static void ProcessingBuiltinTypes(Type type, object value, int? index = null, string prefix = "|-")
+        {
+            if (index == null)
+                Console.WriteLine(string.Format("{0}({1}) \t\tValue:{2}", prefix, type.Name, value));
+            else
+                Console.WriteLine(string.Format("{0}{1}({2}) \t\tValue:{3}", prefix, index, type.Name, value));
+        }
+
+        static void ProcessingClassTypes(PropertyInfo propertyInfo, object data, string prefix = "|-")
+        {
+            Type classType = propertyInfo.PropertyType;
+            var classDatas = propertyInfo.GetValue(data);
+            PropertyInfo[] classPropertyInfos = classType.GetProperties();
+
+            Console.WriteLine(string.Format("{0}{1}({2}(Class))", prefix, propertyInfo.Name, propertyInfo.PropertyType.Name));
+
+            foreach (var classProp in classPropertyInfos)
+            {
+                Console.WriteLine(string.Format("{0}|-{1}({2}) \t\t\tValue:{3}", prefix, classProp.Name, classProp.PropertyType.Name, classProp.GetValue(classDatas)));
+            }
+        }
+
+        /// <summary>
+        /// for inner Collections
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="data"></param>
+        /// <param name="index"></param>
+        /// <param name="prefix"></param>
+        static void ProcessingClassTypes(Type type, object data, int? index = null, string prefix = "|-")
+        {
+            PropertyInfo[] classPropertyInfos = type.GetProperties();
+
+            Console.WriteLine(string.Format("{0}({1})({2}(Class))", prefix, index, type.Name));
+
+            foreach (var classProp in classPropertyInfos)
+            {
+                Console.WriteLine(string.Format("{0}|-{1}({2}) \t\t\tValue:{3}", prefix, classProp.Name, classProp.PropertyType.Name, classProp.GetValue(data)));
+            }
+        }
+
+        static void ProcessingGenericCollection(PropertyInfo propertyInfo, object data, string prefix = "|-")
+        {
+            var dataICollection = propertyInfo.GetValue(data) as System.Collections.ICollection;
+            Type typeInner = propertyInfo.PropertyType.GetGenericArguments()[0];
+
+            Console.WriteLine(string.Format("{0}{1}({2}<{3}>) \t\tCount:{4}", prefix, propertyInfo.Name, propertyInfo.PropertyType.Name, typeInner.Name, dataICollection.Count));
+
+            switch (typeInner.Namespace)
+            {
+                case "System":
+                    // Built-in types collection
+
+                    int i = 0;
+                    foreach (var dataChild in dataICollection)
+                    {
+                        ProcessingBuiltinTypes(typeInner, dataChild, i, prefix + prefix);
+                        i++;
+                    }
+                    break;
+
+                case PROJECT_NAME:
+                    // User defined classes collection
+                    int j = 0;
+                    foreach (var dataChild in dataICollection)
+                    {
+                        ProcessingClassTypes(typeInner, dataChild, j);
+                        j++;
+                    }
+                    break;
+
+                case "System.Collections.Generic":
+                    // Collection types collection
+                    ProcessingGenericCollection(propertyInfo, data);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+    }
+}
